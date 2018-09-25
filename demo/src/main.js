80601939253965e4b09599bfab0bb0e357b2d035
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-09-25 11:21:34 +0800

    mod: Demo

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,26 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import VueResource from 'vue-resource';
+import axios from 'axios';
 import { sync } from 'vuex-router-sync';
 import App from './App';
 import router from './router';
 import store from './store';
 import auth from '../../src';
 
-Vue.use(VueResource);
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 Vue.store = store;
 Vue.router = router;
-
 
 Vue.use(auth, {
   fetchUser: '/api/auth/me',
   appKey: process.env.APP_KEY,
 });
 
-
 sync(store, router);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
